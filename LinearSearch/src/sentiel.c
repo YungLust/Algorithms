dd9ed7b1553d49d4fce7ea9ffc2ab62cec61598e
YungLust <artyr.yanchuk2@gmail.com>
2024-09-22 14:54:34 +0300

    Changed structure of project now .c files are in src and machine codes are in out

---
+++ LinearSearch/src/sentiel.c
@@ -0,0 +1,15 @@
+#include "./lib/utils.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int alg(int arr[], int n, int key) {
+  int res;
+  return res;
+}
+
+int main() {
+  int n = 25;
+  int arr[n];
+  generate(arr, 25);
+  printIntArr(arr, 25);
+}
