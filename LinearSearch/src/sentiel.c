8102f43fec95e363dca62db73e76efe73155e705
YungLust <artyr.yanchuk2@gmail.com>
2024-10-04 12:10:47 +0300

    Changed for-loop to while-loop, there was unnecessary check i<n. Also fixed mistake on line 7, arr[n-1] was assigned to uninitialized variable 'last'. It created segmentation fault.

--- LinearSearch/src/sentiel.c
+++ LinearSearch/src/sentiel.c
@@ -4,12 +4,10 @@
 int alg(int arr[], int n, int key) {
   int res;
   int last;
-  arr[n - 1] = last; // save the last element of arr
-  int i;
-  for (i = 0; i < n; i++) {
-    if (arr[i] == key) {
-      break; // if we found the key we end for-loop
-    }
+  last = arr[n - 1]; // save the last element of arr
+  int i = 0;
+  while (arr[i] != key) { // if we found key we end while-loop
+    i++;
   }
 
   // we can merge conditions because
@@ -23,11 +21,11 @@ int alg(int arr[], int n, int key) {
 }
 
 int main() {
-  int n = 25;
+  int n = 50;
   int key = 10;
   int arr[n];
-  generate(arr, 25, 15);
-  printIntArr(arr, 25);
+  generate(arr, n, n + 100);
+  printIntArr(arr, n);
   int res = alg(arr, n, key);
   if (res > 0) {
     printf("The value of [%d] was found with the index [%d]\n", key, res);
