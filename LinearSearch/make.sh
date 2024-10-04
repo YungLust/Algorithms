31d4dab828b6861c13aeda528e03f0d46b1560ff
YungLust <artyr.yanchuk2@gmail.com>
2024-10-04 11:56:04 +0300

    Now cheks if gcc is completed successfully. Now executes compiled file. If -t provided will time execution of compiled file

--- LinearSearch/make.sh
+++ LinearSearch/make.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/zsh
 if [ -n "$1" ]; then
-  echo "compilling $1.c"
+  echo "Compilling $1.c"
   gcc -Werror -I ./lib ./src/$1.c -L ./lib -l utils -o ./out/$1
+  #check if gcc ended well
+  if [ $? -ne 0 ]; then # '$?' the output of the last commnad
+    echo "Compilation failed!"
+    exit 1
+  else
+    echo "Compiled succesful"
+    if [ "$2" = "-t" ]; then
+      echo "Starting & timing the file out/$1"
+      time out/./$1
+    else
+      echo "Starting the file out/$1"
+      out/./$1
+    fi
+  fi
+
+  
 else
   echo "Put the name of the file!"
 fi
