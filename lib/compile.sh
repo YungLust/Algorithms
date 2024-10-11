9cbf2fdccdd72e6553b6cfe8d3c11a278adb4ba8
YungLust <artyr.yanchuk2@gmail.com>
2024-10-11 21:36:46 +0300

    Fixed some echoes, misspelled some words

--- lib/compile.sh
+++ lib/compile.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/zsh
-  echo "Compilling utils.c to utils.o"
+  echo "Compiling utils.c to utils.o"
   gcc -c utils.c
   #check if gcc ended well
   if [ $? -ne 0 ]; then # '$?' the output of the last commnad
     echo "Compilation failed!"
     exit 1
   else
-    echo "Compiled succesful\n"
+    echo "Compiled successful\n"
     echo "Archiving utils.o to libutils.a"
     ar rcs libutils.a utils.o
     if [ $? -ne 0 ]; then
       echo "Archiving failed!"
       exit 1
     else
-      echo "Succesful archivation\n"
+      echo "Successful archivation\n"
       echo "Library is ready to use!"
     fi
   fi
