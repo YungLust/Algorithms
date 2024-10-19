c4739be9a0af7abf9f900614177956f25c097e26
YungLust <artyr.yanchuk2@gmail.com>
2024-10-19 14:32:22 +0300

    Added bubble sort and tested it in Main

--- Sorting/src/Main.java
+++ Sorting/src/Main.java
@@ -1,5 +1,18 @@
-public class Main{
-    public static void main(String[] args){
+import java.util.Arrays;
 
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] genNumbers = Generate.intArray(30,150);
+        System.out.println(Arrays.toString(genNumbers));
+
+        //create sorter with buble sort strategy
+        Sorter sorter = new Sorter(new BubbleSort());
+
+        //now we can sort
+        sorter.sort(genNumbers);
+
+        System.out.println(Arrays.toString(genNumbers));
     }
 }
