53afb2f7b15a482e3c44fc64db853d330749a3f1
YungLust <artyr.yanchuk2@gmail.com>
2024-10-25 23:56:28 +0300

    Added sorting strategy - insertion algorithm

---
+++ Sorting/src/InsertionSort.java
@@ -0,0 +1,32 @@
+public class InsertionSort implements SortStrategy {
+    private int operationNumber;
+
+    @Override
+    public void sort(int[] arr) {
+        int n = arr.length;
+        operationNumber += 1;
+        //we need to start from 1 to skip 0 index being compared to nothing
+        for (int i = 1; i < n; i++) {
+            operationNumber += 3;
+            //we take the last element of sorted part of array (which bound is i)
+            for (int j = i - 1; j >= 0; j--) {
+                operationNumber += 4;
+                //in this scenario we will call j current and j+1 next
+                int next = j + 1;
+                // if current is greater than next we swap them
+                if (arr[j] > arr[next]) {
+                    int temp = arr[next];
+                    arr[next] = arr[j];
+                    arr[j] = temp;
+                    operationNumber += 3;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    public int getOperationNumber() {
+        return operationNumber;
+    }
+}
